2581c3935dfb585d949216581b2be880649b6741
Harsha <harsha.pakanathi@gmail.com>
2024-12-08 13:00:07 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -4,5 +4,5 @@ module "ec2" {
     # You can also create variables or else you can directly use values here
     ami_id = var.ami_something
     instance_type = "t3.small"
-    security_group_ids = ["sg-07984f846f890ba78"]
+    security_group_ids = ["sg-00e046ce0faad1363"]
 }
